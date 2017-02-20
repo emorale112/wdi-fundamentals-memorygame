5e9def112382f021212a1cbbf0ed4cb492dcd359
Erik Morales <Erik Morales>
2017-02-20 17:01:25 -0600

    created if else

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,11 @@ var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+} else if (cardOne === cardThree) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again");
+}
